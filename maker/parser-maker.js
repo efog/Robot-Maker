8178093532e18379abf84255b99fd7e5d0507c6e
Etienne Brouillard <etienne.brouillard@outlook.com>
2016-07-08 11:15:10 -0400

    ESLint rule updates

--- maker/parser-maker.js
+++ maker/parser-maker.js
@@ -1,11 +1,13 @@
 var model = module.exports,
     pegjs = require('pegjs');
+
 /**
  * Make parser off of grammar
  * @param grammar grammar content as string
  */
-function make(grammar) {
+function make (grammar) {
     var parser = pegjs.buildParser(grammar);
+
     return parser;
 }
 model.make = make;
